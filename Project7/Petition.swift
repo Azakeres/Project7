120be6b92b884a9929c9c07574e0edf387448b1d
Arash Zakeresfahani <azakeres@iu.edu>
2019-03-26 21:38:18 -0400

    100DaysOfSwift project7

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Arash Zakeresfahani on 3/26/19.
+//  Copyright © 2019 Arash Zakeresfahani. All rights reserved.
+//
+
+import Foundation
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,14 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Arash Zakeresfahani on 3/26/19.
+//  Copyright © 2019 Arash Zakeresfahani. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    var results: [Petition]
+    
+}
